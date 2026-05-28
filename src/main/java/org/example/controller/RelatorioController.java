c69e720b036779025f166f2d3fccc0192248d0c4
Keetta <nicholasfelixloko2@gmail.com>
2026-05-28 13:30:48 -0300

    Adicionado Controller do Relatorio

--- src/main/java/org/example/controller/RelatorioController.java
+++ src/main/java/org/example/controller/RelatorioController.java
@@ -1,4 +1,78 @@
 package org.example.controller;
 
+import org.example.model.service.PedidoService;
+import org.example.view.RelatorioView;
+
+import javax.swing.*;
+import java.math.BigDecimal;
+import java.util.Map;
+
 public class RelatorioController {
+    private final RelatorioView view;
+    private final PedidoService pedidoService;
+
+    public RelatorioController(
+            RelatorioView view,
+            PedidoService pedidoService
+    ) {
+        this.view = view;
+        this.pedidoService = pedidoService;
+    }
+
+    public void atualizarRelatorio() {
+        try {
+            BigDecimal faturamento =
+                    pedidoService.calcularFaturamentoTotal();
+            Long totalPedidos =
+                    pedidoService.contarVendasRealizadas();
+            Map<String, BigDecimal> vendas =
+                    pedidoService.getVendasPorCategoria();
+
+            view.atualizarCards(
+                    faturamento,
+                    totalPedidos
+            );
+
+            view.limparSaidas();
+            if (vendas.isEmpty()) {
+                view.adicionarMensagemSemVendas();
+                return;
+            }
+
+            vendas.forEach((categoria, quantidade) -> {
+                String nomeExibicao =
+                        categoria.equalsIgnoreCase("peso")
+                                ? "Buffet"
+                                : categoria;
+
+                String sufixo =
+                        nomeExibicao.equalsIgnoreCase("Buffet")
+                                ? " Kg"
+                                : " Unid.";
+
+                String valorFormatado =
+                        sufixo.contains("Kg")
+                                ? String.format(
+                                "%.3f",
+                                quantidade
+                        )
+                                : String.valueOf(
+                                quantidade.intValue()
+                        );
+
+                view.adicionarItemSaida(
+                        nomeExibicao,
+                        valorFormatado + sufixo
+                );
+            });
+            view.refresh();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(
+                    view,
+                    e.getMessage(),
+                    "Erro",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+    }
 }
